02a743e417cdf8490a778f157614f2554021a10e
Pauldev18 <vuquanganh1802@gmail.com>
2023-12-18 15:11:46 +0700

    login

--- src/main/java/com/WebXemPhim/WebXemPhim/Controller/AccountController.java
+++ src/main/java/com/WebXemPhim/WebXemPhim/Controller/AccountController.java
@@ -1,7 +1,31 @@
 package com.WebXemPhim.WebXemPhim.Controller;
 
+import com.WebXemPhim.WebXemPhim.DTO.AccountDTO;
+import com.WebXemPhim.WebXemPhim.DTO.Login;
+import com.WebXemPhim.WebXemPhim.Entity.Users;
+import com.WebXemPhim.WebXemPhim.Service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AccountController {
+    private AccountService accountService;
+
+    @Autowired
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+    @PostMapping("/login")
+    public ResponseEntity<Object> login(@RequestBody Login login){
+        AccountDTO users = accountService.login(login.getUsername(), login.getPassword());
+        if(users != null){
+            return new ResponseEntity<>(users, HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>("tài khoản hoặc mật khẩu không chính xác", HttpStatus.BAD_REQUEST);
+        }
+    }
 }
